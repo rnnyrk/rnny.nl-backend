52c276059ef00a1e51df0080eeb91e7bc3591c60
Ronny Rook <info@ronnyrook.nl>
2019-03-09 12:38:21 +0100

    Resolve json with TypeScript and send mail via Nodemailer

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import nodemailer from 'nodemailer';
 
-import secrets from 'config/secrets';
+import secrets from 'config/secrets.json';
 
 const app = express();
 const port = process.env.PORT || 8888;
@@ -20,31 +20,43 @@ app.use(cors({
   origin: 'http://localhost:3000',
 }));
 
-app.post('/mail', async (req, res) => {
-  console.log(req.body);
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: secrets.email,
+    pass: secrets.password,
+  }
+});
 
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-      user: secrets.email,
-      pass: secrets.password,
+app.post('/mail', async (req, res) => {
+  const values = req.body;
+
+  transporter.verify((error: string) => {
+    if (error) {
+      console.error('error', error);
+      res.status(500).json({ error: 'Error sending the message' });
     }
   });
 
-  // const mailOptions = {
-  //   from: '"Website" <name@website.com>',
-  //   to: "info@ronnyrook.nl",
-  //   subject: "Hello from ronnyrook.nl",
-  //   html: "<b>Hello world?</b>" // html body
-  // };
+  if (values.name && values.email && values.message) {
+    const mailOptions = {
+      from: `"${values.name}" <${values.email}>`,
+      to: secrets.email,
+      subject: 'Hello from ronnyrook.nl',
+      html: values.message,
+    };
 
-  // // send mail with defined transport object
-  // const info = await transporter.sendMail(mailOptions)
-  // console.log("Message sent: %s", info.messageId);
-
-  res.status(200).send({ response: 'Message was send succesfully.' });
+    await transporter.sendMail(mailOptions, (error: string) => {
+      if (error) {
+        console.error('error', error);
+        res.status(500).json({ error: 'Error sending the message' });
+      } else {
+        res.status(200).json({ message: 'Message was send succesfully' });
+      }
+    });
+  }
 });
 
 app.listen(port, () => {
